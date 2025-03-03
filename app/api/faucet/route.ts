86c107cf7592c1082fece5ec3718117895993930
Montana Wong <montanawong@gmail.com>
2025-03-03 18:08:08 -0500

    throw error if faucet not set

--- app/api/faucet/route.ts
+++ app/api/faucet/route.ts
@@ -5,6 +5,11 @@ export async function POST(req: Request) {
     if (!to || !isAddress(to)) {
         return new Response('Invalid `to` address provided', { status: 400 });
     }
+
+    if (!process.env.NEXT_FAUCET_SERVICE_URL) {
+        return new Response('Faucet service URL is not set', { status: 500 });
+    }
+
     const response = await fetch(`https://${process.env.NEXT_FAUCET_SERVICE_URL}${to}`, {
         method: 'POST',
         body: JSON.stringify({ to }),
